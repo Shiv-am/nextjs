f2c765a911f7ad16ba777b951e2d92e82082261a
Shivam Bansal <shivam2032.cse18@chitkara.edu.in>
2021-06-02 14:15:58 +0530

    adding Links

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,12 +1,15 @@
+import Link from 'next/link'
 const Navbar = () => {
     return ( 
         <nav>
             <div className="logo">
                 <h2>I am a navbar</h2>
             </div>
-            <a>About</a>
+            <Link href='/about'>About</Link>
             <br />
-            <a>Home</a>
+            <Link href="/">Home</Link>
+           <br />
+           <Link href="/ninjas">Ninjas listening</Link>
         </nav>
      );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import Footer from '../comps/Footer'
 import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
@@ -9,6 +10,7 @@ export default function Home() {
     <div >
     <Navbar />
        <h1>Helllooo...</h1>
+       <Link href="/ninjas">See Ninjas listing</Link>
        <Footer />
     </div>
   )
