b8426d8fb477edf39e34feb1b6371add3ea83456
Shivam Bansal <shivam2032.cse18@chitkara.edu.in>
2021-06-02 13:41:52 +0530

    pages and routes

---
+++ pages/ninjas/index.js
@@ -0,0 +1,9 @@
+const Ninjas = () => {
+    return (  
+        <div>
+            <h2>List of people..</h2>
+        </div>
+    );
+}
+ 
+export default Ninjas;
---
+++ pages/ninjas/test.js
@@ -0,0 +1,9 @@
+const Test = () => {
+    return ( 
+     <div>
+         <h2>This is a test page..</h2>
+     </div>
+     );
+}
+ 
+export default Test;
