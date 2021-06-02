666706fe32bcdc1d827c419720521407c285a255
Shivam Bansal <shivam2032.cse18@chitkara.edu.in>
2021-06-02 14:35:59 +0530

    adding Layout component

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import Footer from '../comps/Footer'
-import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div >
-    <Navbar />
        <h1>Helllooo...</h1>
        <Link href="/ninjas">See Ninjas listing</Link>
-       <Footer />
     </div>
   )
 }
